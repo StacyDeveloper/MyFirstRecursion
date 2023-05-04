2c0f7d025a925ba4ea7b2ad0c0f2d6312aca178c
StacyDeveloper <129184938+StacyDeveloper@users.noreply.github.com>
2023-05-04 12:20:08 +0300

    Внесла изменения в код.

--- MyFirstRecursion/Program.cs
+++ MyFirstRecursion/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Напишите что-то");
+        var str = Console.ReadLine();
+
+        Console.WriteLine("Укажите глубину эха");
+        var deep = int.Parse(Console.ReadLine());
+
+        Echo(str, deep);
+
+        Console.ReadKey();
+    }
+
+    static void Echo(string saidword, int deep)
+    {
+        var modif = saidword;
+        if (modif.Length > 2)
+        {
+            modif = modif.Remove(0, 2);
+        }
+        Console.WriteLine("..." + modif);
+        if (deep > 1)
+        {
+            Echo(modif, deep - 1);
+        }
+    }
+}
